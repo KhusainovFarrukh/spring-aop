ceb96dad6235ca8d81f7dd77ec614903d1cb16c9
f_khusainov <farrukh_kh@192.168.1.6>
2024-06-22 00:38:49 +0500

    Added dependencies

---
+++ src/main/java/kh/farrukh/springaop/Main.java
@@ -0,0 +1,5 @@
+package kh.farrukh.springaop;
+
+public class Main {
+
+}
---
+++ src/main/java/kh/farrukh/springaop/firstaspect/FirstAspectApp.java
@@ -0,0 +1,11 @@
+package kh.farrukh.springaop.firstaspect;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class FirstAspectApp {
+
+  public static void main(String[] args) {
+    var context = new AnnotationConfigApplicationContext("kh.farrukh.springaop.firstaspect");
+  }
+
+}
